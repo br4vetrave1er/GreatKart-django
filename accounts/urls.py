4117c3125e59cb5742f1426e06e9a7d415eb45a3
br4vetrave1er <yadavakshay36912@gmail.com>
2023-10-31 11:39:44 +0530

    added password reset funcnalities

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('login/', views.login, name='login'),
 
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
+    path('forgotPassword/', views.forgot_password, name='forgot_password'),
+    path('resetPassword/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
+    path('resetPassword/', views.reset_password, name='reset_password')
 ]
